53bc09983d782b28c76a4b47d1db7a149fb69a3a
rpf13 <rpf13@me.com>
2024-05-10 06:31:48 +0200

    Add assets to project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  # we might want to add the relation below
+  # has_many :submissions
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

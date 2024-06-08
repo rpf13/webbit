02d6cba95726f4bd8fc1b79da22acb8d63b36122
rpf13 <rpf13@me.com>
2024-06-08 12:18:07 +0200

    Add custom css file and adjust styling

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
+    require('postcss-import'),
     require('tailwindcss/nesting'),
+    require('tailwindcss'),
     require('autoprefixer'),
   ],
 }

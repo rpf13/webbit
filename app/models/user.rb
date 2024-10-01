c22e4ae83512ecc0a1d8f05ca32ec5a88ce23f54
rpf13 <rpf13@me.com>
2024-10-01 07:46:54 +0200

    Add first part of comments section

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # we might want to add the relation below
   has_many :submissions, dependent: :destroy
   has_many :communities
+  has_many :comments, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
